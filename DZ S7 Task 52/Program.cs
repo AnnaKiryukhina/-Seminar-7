f086b07e4adc17a20d789a678b5d40dff2e34139
AnnaKiryukhina <124432443+AnnaKiryukhina@users.noreply.github.com>
2023-03-09 09:58:07 +0700

    Seminar 7 задача52 поиск суммы столбцов

---
+++ DZ S7 Task 52/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int [m, n];
+    Random rand = new Random();
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            matrix[i,j] = i+j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void MeanArithmetic(int[,] matr, int n )
+{
+    
+   int [] array = new int [n];
+   int sum =0;
+   for (int k = 0; k <n; k++ )
+   {for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            if(i < matr.GetLength(0))
+            {
+                array[k] =(sum + matr[i,j])/j ;
+            }  
+        } 
+    }
+    return ;
+    }
+}     
+
+void PrintMeanArithmetic(int[] array )
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+   
+int m = ReadInt("Введите количетсво строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+
+
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+System.Console.WriteLine();
+var array = MeanArithmetic(myMatrix, n );
+PrintMeanArithmetic(array);
+
+
+
+
